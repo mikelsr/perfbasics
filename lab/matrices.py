5f5efcc67753a412e4ec9669fcc9f6205657e22a
mikelsr <mikelsr@pm.me>
2025-06-19 22:47:43 +0200

    Create sample matrices functions

---
+++ lab/matrices.py
@@ -0,0 +1,29 @@
+from random import randint
+
+_COLS = 3000
+_ROWS = 3000
+
+# Create a large square matrix using native Python lists
+print("Creating matrix...", end="")
+MATRIX = [[randint(0, 100) for _ in range(_ROWS)] for _ in range(_COLS)]
+print(" done.")
+
+
+# Function to iterate over the matrix row by row
+def iterate_by_row():
+    total = 0
+    size = len(MATRIX)
+    for col in range(size):
+        for row in range(size):
+            total += MATRIX[col][row]
+    return total
+
+
+# Function to iterate over the matrix column by column
+def iterate_by_column():
+    total = 0
+    size = len(MATRIX)
+    for col in range(size):
+        for row in range(size):
+            total += MATRIX[row][col]
+    return total
